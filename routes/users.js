7a0dd5c04c8113bf5e6f260393184cb774153fa8
Kate Santo <katerine.santo@gmail.com>
2020-02-28 17:47:34 +0000

    Replacing local data with existing API data

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,38 @@
-const users = require('../data/users.json');
+// const users = require('../data/users.json');
+const axios = require('axios');
 
 const userRoutes = (app) => {
-
-    app.get('/users/:city', (req, res) => {
-        if (req.params.city.toUpperCase() === "London".toUpperCase()) {
-            filteredUsers = users.filter(checkUsers);
-
+    axios.get('https://bpdts-test-app.herokuapp.com/users')
+        .then((response) => {
+            let filteredUsers = response.data.filter(checkUsers);
             function checkUsers(user) {
                 if (((user.latitude >= 50.82) && (user.latitude <= 52.22)) && ((user.longitude >= -1.29) && (user.longitude <= 0.92))) {
                     return user;
                 }
-
             }
-            res.send(filteredUsers);
-        } else {
-            res.status(400).send('This search is only available for London');
-        }
-    });
-
+            console.log(filteredUsers);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
-
 module.exports = userRoutes;
+
+
+
+   // app.get('/users/:city', (req, res) => {
+    //     if (req.params.city.toUpperCase() === "London".toUpperCase()) {
+    //         filteredUsers = users.filter(checkUsers);
+
+    //         function checkUsers(user) {
+    //             if (((user.latitude >= 50.82) && (user.latitude <= 52.22)) && ((user.longitude >= -1.29) && (user.longitude <= 0.92))) {
+    //                 return user;
+    //             }
+
+    //         }
+    //         res.send(filteredUsers);
+    //     } else {
+    //         res.status(400).send('This search is only available for London');
+    //     }
+    // });
