1577a3251d208f239af0d80fd832343b1b7062e6
Kate Santo <katerine.santo@gmail.com>
2020-03-01 15:51:13 +0000

    Simplifying code and writing test

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
-// const users = require('../data/users.json');
 const axios = require('axios');
 
-const userRoutes = (app) => {
+const userRoutes = () => {
     axios.get('https://bpdts-test-app.herokuapp.com/users')
         .then((response) => {
             let filteredUsers = response.data.filter(checkUsers);
@@ -11,6 +10,7 @@ const userRoutes = (app) => {
                 }
             }
             console.log(filteredUsers);
+            console.log(response.status);
         })
         .catch((error) => {
             console.log(error);
@@ -18,21 +18,3 @@ const userRoutes = (app) => {
 }
 
 module.exports = userRoutes;
-
-
-
-   // app.get('/users/:city', (req, res) => {
-    //     if (req.params.city.toUpperCase() === "London".toUpperCase()) {
-    //         filteredUsers = users.filter(checkUsers);
-
-    //         function checkUsers(user) {
-    //             if (((user.latitude >= 50.82) && (user.latitude <= 52.22)) && ((user.longitude >= -1.29) && (user.longitude <= 0.92))) {
-    //                 return user;
-    //             }
-
-    //         }
-    //         res.send(filteredUsers);
-    //     } else {
-    //         res.status(400).send('This search is only available for London');
-    //     }
-    // });
