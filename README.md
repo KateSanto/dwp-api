89256e5d9896bc5a16ad1ad1eb8fdae413c25eab
Kate Santo <katerine.santo@gmail.com>
2020-03-02 20:57:24 +0000

    Keeping record of different test approaches + new Readme file

--- README.md
+++ README.md
@@ -11,7 +11,16 @@ This helps process the body of requests sent to the express server
 ## Nodemon
 This acts as a file-watcher, reloading our server when we make changes to our files
 
+## Axios
+This is a Javascript library I've used to make HTTP requests to the existing API. It helped me bring data into my own API and make it available so I could process it.
+
 # Useful commands
 
 ## npm start
 Will launch listening on port 3001
+
+## npm run test
+Will run all tests in the test file
+
+# Testing notes
+The test file (api.test.js) is still work in progress. I have used Jest and supertest to write two tests. One to check that the response status is the expected (200) and another one to check that the data type of the filtered result is the expected (Array). I have tried different approaches (mocking a request, writing async tests, using return/.then statements), but none have worked. I have enjoyed the challenge and have learned a lot while trying out different options, but testing asynchronous code is definitely a space for me to improve on.
--- api.test.js
+++ api.test.js
@@ -1,11 +1,12 @@
-// const app = require('./server');
-// const request = require('supertest');
-// const axios = require('axios');
-// const checkUsers = require('./routes/users');
-// const mockAxios = require('axios');
+const app = require('./server');
+const request = require('supertest');
+const axios = require('axios');
+const checkUsers = require('./routes/users');
+const mockAxios = require('axios');
 const userRoutes = require('./routes/users');
 const supertest = require('supertest');
 const appRouter = require('./routes/routes')
+const response = require('./routes/users');
 
 
 // test('tests the base route and returns "Array" for typeof data', async () => {
@@ -14,13 +15,26 @@ const appRouter = require('./routes/routes')
 //   });
 
 
-describe("Testing API", () => {
-    test("tests the base route and returns 200 for response status", async() => {
-        const response = await supertest(userRoutes).get('/');
-        //need to troubleshoot this. When running API in terminal, status is 200, but test fails.
-        expect(response.status).toEqual(200);
+
+
+test('tests the base route and returns "Array" for typeof data logged after promise is fulfilled and filtered', () => {
+    return response.then(filteredUsers => {
+      expect(typeof filteredUsers).toBe("Array");
     });
-})
+  });
+
+
+
+
+
+  //////////////////
+// describe("Testing API", () => {
+//     test("tests the base route and returns 200 for response status", async() => {
+//         const response = await supertest(userRoutes).get('/');
+//         //need to troubleshoot this. When running API in terminal, status is 200, but test fails.
+//         expect(response.status).toEqual(200);
+//     });
+// })
 
 
 
