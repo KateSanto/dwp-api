1b905bdc9bfb3c6da82521c5ed615cc77338f296
Kate Santo <katerine.santo@gmail.com>
2020-03-02 21:15:35 +0000

    Cleaning up code before sending out link

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // load up the express framework and body-parser helper
 const express = require('express');
-// const bodyParser = require('body-parser');
 
-// create an instance of express to serve our end points
+// create an instance of express to serve our endpoint
 const app = express();
 
 // this is where we'll handle our various routes from
 const routes = require('./routes/routes.js')();
 
-// finally, launch our server on port 3001.
+// finally, launch our server on port 3001
 const server = app.listen(3001, () => {
     console.log('listening on port %s...', server.address().port);
 });
