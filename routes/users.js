441959ae450b397d0baa719dc404d7f5825211af
Kate Santo <katerine.santo@gmail.com>
2020-06-12 11:10:31 +0100

    Note added during interview

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const userRoutes = () => {
     axios.get('https://bpdts-test-app.herokuapp.com/users')
         .then((response) => {
+            //const would have been a better way to declare this variable. I'm not going to assign a different value to it, and, if it's const, the system can alert me if I try to reassign it by mistake
             let filteredUsers = response.data.filter(checkUsers);
             function checkUsers(user) {
                 if (((user.latitude >= 50.82) && (user.latitude <= 52.22)) && ((user.longitude >= -1.29) && (user.longitude <= 0.92))) {
